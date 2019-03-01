8a65f1112ebb1c8b5c15de67aa96f5dc876a0854
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-03-01 10:14:16 -0700

    Adds Views and subclasses

---
+++ ContactsCK/Controller/View Controller/ContactDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ContactDetailViewController.swift
+//  ContactsCK
+//
+//  Created by Dominic Lanzillotta on 3/1/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ContactsCK/Controller/View Controller/ContactListTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ContactListTableViewController.swift
+//  ContactsCK
+//
+//  Created by Dominic Lanzillotta on 3/1/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import UIKit
+
+class ContactListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
