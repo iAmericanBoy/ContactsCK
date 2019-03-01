cf9fb5a5032fc7251170a3f151c2390504344de3
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-03-01 10:33:49 -0700

    Implements DataSource and updateViews Patteren

--- ContactsCK/Controller/View Controller/ContactDetailViewController.swift
+++ ContactsCK/Controller/View Controller/ContactDetailViewController.swift
@@ -9,8 +9,45 @@
 import UIKit
 
 class ContactDetailViewController: UIViewController {
-
+    
+    //MARK: - Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    //MARK: - Properties
+    var contact: Contact?
+    
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
+    }
+    
+    //MARK: - Actions
+
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        guard let name = nameTextField.text, !name.isEmpty,
+            let phone = phoneTextField.text,
+            let email = emailTextField.text else {return}
+        
+        if let contact = contact {
+            //update contact
+            ContactController.shared.update(contact: contact, withName: name, andPhoneNumber: phone, andEmail: email)
+            self.navigationController?.popViewController(animated: true)
+        } else {
+            //save new Conatact
+            ContactController.shared.createContact(withName: name, andEmail: email, andPhoneNumber: phone)
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    //MARK: - Private Functions
+    func updateViews() {
+        guard let contact = contact else {return}
+        self.title = "Contact"
+        self.nameTextField.text = contact.name
+        self.emailTextField.text = contact.email
+        self.phoneTextField.text = contact.phoneNumber
     }
 }
--- ContactsCK/Controller/View Controller/ContactListTableViewController.swift
+++ ContactsCK/Controller/View Controller/ContactListTableViewController.swift
@@ -9,34 +9,45 @@
 import UIKit
 
 class ContactListTableViewController: UITableViewController {
-
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return ContactController.shared.contacts.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
 
-        // Configure the cell...
-
+        cell.textLabel?.text = ContactController.shared.contacts[indexPath.row].name
+        
         return cell
     }
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            let contactToDelete = ContactController.shared.contacts[indexPath.row]
+            ContactController.shared.delete(contact: contactToDelete)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
 
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //IIDOO
+        if segue.identifier == "toDetailVC" {
+            guard let index = tableView.indexPathForSelectedRow else {return}
+            if let destinationVC = segue.destination as? ContactDetailViewController {
+                destinationVC.contact = ContactController.shared.contacts[index.row]
+            }
+        }
     }
 }
